896649aa7daa13fca5ca6b03f59d79cfc650fb72
Wang <gavin19950511@gmail.com>
2024-01-02 01:26:56 +0000

    update(vec search): update

---
+++ api/initRedis.py
@@ -0,0 +1,111 @@
+import redis
+from redis.commands.search.indexDefinition import (
+    IndexDefinition,
+    IndexType
+)
+from redis.commands.search.query import Query
+from redis.commands.search.field import (
+    TextField,
+    VectorField
+)
+import pandas as pd
+import numpy as np
+import requests
+from typing import List
+from sentence_transformers import SentenceTransformer
+import re
+def embedding(text):
+    model = SentenceTransformer('sentence-transformers/all-distilroberta-v1')
+    return model.encode(text)
+
+
+
+REDIS_HOST =  "127.0.0.1"
+REDIS_PORT = 6379
+REDIS_PASSWORD = "" # default for passwordless Redis
+
+# Connect to Redis
+redis_client = redis.Redis(
+    host=REDIS_HOST,
+    port=REDIS_PORT,
+    password=REDIS_PASSWORD
+)
+assert(redis_client.ping())
+
+# load data and convert vec to number from string
+
+texts = []
+windowsSize = 1
+with open("./source.txt") as f:
+    for line in f.readlines():
+        line = line.strip()
+        if line == "":
+            continue
+        # print("text:",line)
+        subString = [i for i in re.split('，|;|\'|\?|\~|!|&|=|。|；',line) if len(i)>1]
+        # print(f"subString: {subString}")
+        texts.extend(["，".join(subString[idx-windowsSize:idx+windowsSize]) for idx in range(windowsSize,len(subString)-windowsSize)])
+# print(f"texts: {texts}")
+output = [ embedding(sentence) for sentence in texts]
+
+embeddings = pd.DataFrame(output)
+# print(f"len(text): {len(texts)}")
+# print(embeddings)
+
+mainBoard = []
+for idx in range(len(texts)):
+    vec = list(embeddings.iloc[idx])
+    text = texts[idx]
+    mainBoard.append([idx,text,vec])
+
+data = pd.DataFrame(mainBoard,columns=("id", "title", "vec"))
+# print(f"data:\n{data}")
+
+# Create Redis Table
+VECTOR_DIM = len(data['vec'][0]) # length of the vectors
+VECTOR_NUMBER = len(data)                 # initial number of vectors
+INDEX_NAME = "embeddings-index"           # name of the search index
+PREFIX = "doc"                            # prefix for the document keys
+DISTANCE_METRIC = "COSINE"                # distance metric for the vectors (ex. COSINE, IP, L2)
+VECTOR_COLUME_NAME = "vec"
+CONTENT_COLUME_NAME = "title"
+TABLE_COLUME_NAME = "tableName"
+print(f"VECTOR_DIM: {VECTOR_DIM}")
+
+# Define RediSearch fields for each of the columns in the dataset
+text = TextField(name=CONTENT_COLUME_NAME)
+tableName = TextField(name=TABLE_COLUME_NAME)
+text_embedding = VectorField(VECTOR_COLUME_NAME,
+    "FLAT", {
+        "TYPE": "FLOAT32",
+        "DIM": VECTOR_DIM,
+        "DISTANCE_METRIC": DISTANCE_METRIC,
+        "INITIAL_CAP": VECTOR_NUMBER,
+    }
+)
+fields = [text, tableName, text_embedding]
+
+# Check if index exists
+try:
+    redis_client.ft(INDEX_NAME).info()
+    print("Index already exists")
+except:
+    # Create RediSearch Index
+    redis_client.ft(INDEX_NAME).create_index(
+        fields = fields,
+        definition = IndexDefinition(prefix=[PREFIX], index_type=IndexType.HASH)
+)
+
+def index_documents(client: redis.Redis, prefix: str, documents: pd.DataFrame):
+    records = documents.to_dict("records")
+    for doc in records:
+        key = f"{prefix}:{str(doc['id'])}"
+
+        # replace list of floats with byte vectors
+        doc[VECTOR_COLUME_NAME] = np.array(doc[VECTOR_COLUME_NAME]).astype(np.float32).tobytes()
+        doc['tableName'] = prefix
+        # print(f"size: {len(doc[VECTOR_COLUME_NAME])}")
+        # print(f"doc:\n{doc}")
+        client.hset(key, mapping = doc)
+index_documents(redis_client, PREFIX, data)
+print(f"Loaded {redis_client.info()['db0']['keys']} documents in Redis search index with name: {INDEX_NAME}")
---
+++ api/main.py
@@ -0,0 +1,82 @@
+from typing import Union
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI
+from pydantic import BaseModel
+import initRedis
+from sentence_transformers import SentenceTransformer
+import pandas as pd
+import numpy as np
+from redis.commands.search.query import Query
+import redis
+from initRedis import index_documents
+import uuid
+app = FastAPI()
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+
+
+
+class TextObject(BaseModel):
+    tableName: str = "tableName"
+    text: str = "text"
+
+def embedding(text):
+    model = SentenceTransformer('sentence-transformers/all-distilroberta-v1')
+    return model.encode(text)
+
+
+# Connect to Redis
+REDIS_HOST =  "127.0.0.1"
+REDIS_PORT = 6379
+REDIS_PASSWORD = "" # default for passwordless Redis
+INDEX_NAME = "embeddings-index"           # name of the search index
+
+redis_client = redis.Redis(
+    host=REDIS_HOST,
+    port=REDIS_PORT,
+    password=REDIS_PASSWORD
+)
+assert(redis_client.ping())
+
+
+
+@app.get("/texts/")
+def read_item(tableName: str = "doc", text: str = "文章來源"):
+    #vectorize the query
+    k=5
+    vector_field = "vec"
+    query_vector = embedding(text).astype(np.float32).tobytes()
+
+    #prepare the query
+    sql = f'(@tableName:{tableName})=>[KNN {k} @{vector_field} $vec_param AS vector_score]'
+    # sql = f'(*)=>[KNN {k} @{vector_field} $vec_param AS vector_score]'
+    q = Query(sql).sort_by('vector_score').paging(0,k).dialect(2)
+    print(f"sql:{sql}")
+    
+    params_dict = {"vec_param": query_vector}
+    #Execute the query
+    results = redis_client.ft(INDEX_NAME).search(q, query_params = params_dict)
+    # for i, article in enumerate(results.docs):
+    #     print(f"{i}. {article.title} (Score: {round(1 - float(article.vector_score) ,3) })")
+    #     print(article)
+    return {"tableName": tableName, "text": text, "similar": [(article.title, 1 - float(article.vector_score)) for i, article in enumerate(results.docs)]}
+
+
+
+@app.post("/texts/")
+async def create_item(obj: TextObject):
+    # mainBoard = [[str(uuid.uuid4()),obj.text,embedding(obj.text).astype(np.float32).tobytes()]]
+    # index_documents(redis_client, obj.tableName, pd.DataFrame(mainBoard,columns=("id", "title", "vec")))
+
+    return obj
